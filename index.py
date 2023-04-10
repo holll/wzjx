2c75509042559c47d78b3e7c7d333b219d21002e
hollc <380460708@qq.com>
2023-04-10 14:05:34 +0800

    随机选择下载链接

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import hashlib
 import json
 import os
 import platform
+import random
 import re
 import sys
 import time
@@ -111,28 +112,28 @@ async def jiexi(url):
         return
 
     # 对rosefile特殊处理，down-node使用的是sharepoint，速度更快
-    down_link = aria2_link[0]
-
+    # down_link = aria2_link[0]
+    down_link = random.choice(aria2_link)
     # down-node偶尔会出现错误，增加重试机制
     # 当程序第二次解析同一个链接时，禁用down-node节点
-    if os.environ['rosefile'] is None:
-        try:
-            _ = os.environ[url]
-            isFirst = False
-        except KeyError:
-            isFirst = True
-        # isFirst = False
-        if 'rosefile' in url and isFirst:
-            for temp_url in aria2_link:
-                if 'down-node' in temp_url:
-                    down_link = temp_url
-                    # 赋值任意数据均可，目的是让环境变量不为空
-                    os.environ[url] = '1'
-                    break
-    else:
-        for temp_url in aria2_link:
-            if 'down-node' not in temp_url:
-                down_link = temp_url
+    # if os.environ['rosefile'] is None:
+    #     try:
+    #         _ = os.environ[url]
+    #         isFirst = False
+    #     except KeyError:
+    #         isFirst = True
+    #     # isFirst = False
+    #     if 'rosefile' in url and isFirst:
+    #         for temp_url in aria2_link:
+    #             if 'down-node' in temp_url:
+    #                 down_link = temp_url
+    #                 # 赋值任意数据均可，目的是让环境变量不为空
+    #                 os.environ[url] = '1'
+    #                 break
+    # else:
+    #     for temp_url in aria2_link:
+    #         if 'down-node' not in temp_url:
+    #             down_link = temp_url
     url_domain = re.search('(http|https)://(www.)?(\w+(\.)?)+', down_link).group()
     print(f'获取下载链接{url_domain}/...成功\n{end_time}，请记得及时续费', flush=True)
     return down_link
